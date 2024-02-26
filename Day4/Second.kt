0fd994cf9bc8ffbc2790b08c2243587bac4650dc
Sanidhya Jain <sanidhyajain45@gmail.com>
2024-02-26 11:44:09 +0530

    Null in kotlin

--- Day4/Second.kt
+++ Day4/Second.kt
@@ -1,18 +1,23 @@
 package Day4
+class Second {
 
-        fun printMessage(message: String)
-        {
-            println( message)
-        }
-        fun printMessageWithPrefix(message: String, prefix:String= "Info")
-        {
-            println("$prefix" + " " + message )
-        }
-        fun sum(a:Int, b:Int): Int
-        {
-            return a+b;
-        }
-        fun multiply(a:Int, b:Int)= a * b;
+}
+
+
+
+    fun printMessage(message: String) {
+        println(message)
+    }
+
+    fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+        println("$prefix" + " " + message)
+    }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    fun multiply(a: Int, b: Int) = a * b;
 
 
 fun main()
@@ -22,4 +27,14 @@ fun main()
             printMessageWithPrefix("Hey")
             println(sum(5,6));
             println(multiply(4,5));
+
+            var firstName:String ="This is non null name";
+            firstName="Ayush"
+
+//            firstName=null; throws error...by default non null
+            var MiddleName:String?="This can become null because of ?";
+            MiddleName=null;
+            var LastName:String= "This cant be null";
+            LastName="Jain"
+            println(firstName + MiddleName + LastName)
         }
